3c8f534d3f5b51ceb7f79ed5b576c94ab7042a82
Lenninlasd <lenninlasd@gmail.com>
2018-04-14 18:52:59 -0500

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+        filename: './confort.min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
