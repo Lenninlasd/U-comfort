baf6d5db58a72599dd9058b9e56e59a479f456c5
David Villa <david.villanueva.dev@gmail.com>
2019-04-06 16:48:56 -0500

    Add redux devtools

--- src/main.js
+++ src/main.js
@@ -2,27 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers/root.js';
 import initState from './model.js';
 import { App } from './components/App';
 import enrichData from './enrichData.js';
 
-const store = createStore(rootReducer, {
-  vidrios: initState.elementos.vidrios,
-  paredes: initState.elementos.paredes,
-  techo: initState.elementos.techo,
-  puertas: initState.elementos.puertas,
-  piso: initState.elementos.piso,
-  luces: initState.elementos.luces,
-  depth: initState.depth,
-  width: initState.width,
-  height: initState.height,
-  numberOfPeople: initState.numberOfPeople,
-  exterior: initState.exterior,
-  recinto: initState.recinto,
-  cargaPico: initState.cargaPico
-});
+const store = createStore(
+  rootReducer,
+  {
+    vidrios: initState.elementos.vidrios,
+    paredes: initState.elementos.paredes,
+    techo: initState.elementos.techo,
+    puertas: initState.elementos.puertas,
+    piso: initState.elementos.piso,
+    luces: initState.elementos.luces,
+    depth: initState.depth,
+    width: initState.width,
+    height: initState.height,
+    numberOfPeople: initState.numberOfPeople,
+    exterior: initState.exterior,
+    recinto: initState.recinto,
+    cargaPico: initState.cargaPico
+  },
+  composeWithDevTools()
+);
 
 enrichData(store.dispatch);
 
