d1bc1a9af96088fcebf13e97dc3e91dae3d6233c
Lenninlasd <lenninlasd@gmail.com>
2019-04-16 09:44:37 -0500

    Add config test

--- src/reducers/config.test.js
+++ src/reducers/config.test.js
@@ -1,7 +1,48 @@
-import { appConfig } from './config.js';
+import { appConfig } from './config';
+import * as actions from '../actions';
 
-describe('todos reducer', () => {
+describe('config reducers', () => {
   it('should return the initial state', () => {
     expect(appConfig(undefined, {})).toEqual({});
+  });
+
+  it('should handle SHOW_ELEMENTS_VIEW', () => {
+    expect(
+      appConfig(
+        {},
+        {
+          type: actions.SHOW_ELEMENTS_VIEW,
+          elementType: 'walls'
+        }
+      )
+    ).toEqual({
+      windowsView: 'wallsView'
+    });
+  });
+
+  it('should handle SHOW_ELEMENTS_VIEW with default element view', () => {
+    expect(
+      appConfig(
+        {},
+        {
+          type: actions.SHOW_ELEMENTS_VIEW
+        }
+      )
+    ).toEqual({
+      windowsView: 'glassView'
+    });
+  });
+
+  it('should handle HIDE_ELEMENTS_VIEW', () => {
+    expect(
+      appConfig(
+        {},
+        {
+          type: actions.HIDE_ELEMENTS_VIEW
+        }
+      )
+    ).toEqual({
+      windowsView: null
+    });
   });
 });
